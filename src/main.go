8925d416fd6a2b8bbd44dcb0ae62d56385a702f3
Hugo <hugo.bollon@gmail.com>
2020-08-12 18:49:27 +0200

    Server creation and run with mux router

--- src/main.go
+++ src/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-    fmt.Printf("hello, world\n")
+	// Declare mux router and out port
+	router := mux.NewRouter()
+	port := getPort()
+
+	// Declare the HTTP methods for our routes
+	router.HandleFunc("/", indexHandler).Methods("GET")
+	err := http.ListenAndServe(port, router)
+
+	// Error handling on server startup
+	if err != nil {
+		log.Fatal("Error on server startup: ", err)
+	}
+}
+
+// Manage index route (/)
+func indexHandler(rw http.ResponseWriter, request *http.Request) {
+	fmt.Fprint(rw, "Server is up and functional !")
+}
+
+// Return port
+func getPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":3500"
+		fmt.Printf("WARNING : Port not defined, using default port (%s)\n", port)
+	}
+	return port
 }
