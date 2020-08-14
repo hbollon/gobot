21a02023310a9a35cb1a642e71dfeb7368f9438c
Hugo <hugo.bollon@gmail.com>
2020-08-14 16:53:39 +0200

    Callback and Messaging related types

---
+++ src/messaging/types.go
@@ -0,0 +1,41 @@
+package messaging
+
+/**
+*	All necessary types for building response through Messenger API webhook
+*	It strictly respect `messages` event standards for attributes naming and order
+ */
+
+type User struct {
+	ID string `json:"id,omitempty"`
+}
+
+type Messaging struct {
+	Sender    User    `json:"sender,omitempty"`
+	Recipient User    `json:"recipient,omitempty"`
+	Timestamp int     `json:"timestamp,omitempty"`
+	Message   Message `json:"message,omitempty"`
+}
+
+type Message struct {
+	MID        string `json:"mid,omitempty"`
+	Text       string `json:"text,omitempty"`
+	QuickReply *struct {
+		Payload string `json:"payload,omitempty"`
+	} `json:"quick_reply,omitempty"`
+	Attachments *[]Attachment `json:"attachments,omitempty"`
+	Attachment  *Attachment   `json:"attachment,omitempty"`
+}
+
+type Attachment struct {
+	Type    string  `json:"type,omitempty"`
+	Payload Payload `json:"payload,omitempty"`
+}
+
+type Response struct {
+	Recipient User    `json:"recipient,omitempty"`
+	Message   Message `json:"message,omitempty"`
+}
+
+type Payload struct {
+	URL string `json:"url,omitempty"`
+}
--- src/webhooks.go
+++ src/webhooks.go
@@ -4,11 +4,22 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	Messaging "gobot/src/messaging"
 )
 
 var config Config // Config struct instance
 var tokenInit = getToken()
 var jsonData = json.Unmarshal([]byte(tokenInit), &config)
+
+type Callback struct {
+	Object string `json:"object,omitempty"`
+	Entry  []struct {
+		ID        string                `json:"id,omitempty"`
+		Time      int                   `json:"time,omitempty"`
+		Messaging []Messaging.Messaging `json:"messaging,omitempty"`
+	} `json:"entry,omitempty"`
+}
 
 // Handle GET request made to webhook
 func whGetHandler(writter http.ResponseWriter, request *http.Request) {
@@ -26,3 +37,18 @@ func whGetHandler(writter http.ResponseWriter, request *http.Request) {
 		fmt.Fprint(writter, "Tokens don't match")
 	}
 }
+
+// Handle POST request made to webhook
+// It serialize request content into Callback and call processMassage for response
+/* func whPostHandler(writter http.ResponseWriter, request *http.Request) {
+	var callback Callback
+
+	// Check if req token match with config file token
+	if reqToken == token {
+		writter.Header().Set("Content-Type", "application/json")
+		writter.WriteHeader(http.StatusOK)
+		writter.Write([]byte(reqChallenge))
+	} else {
+		fmt.Fprint(writter, "Tokens don't match")
+	}
+} */
