19d16570016e18834d7c3d9cf9ebceca1e0dc8d8
Hugo <hugo.bollon@gmail.com>
2020-08-15 18:15:43 +0200

    Implemented Levenshtein distance algorithm for message analysis

--- src/analysis/messageAnalysis.go
+++ src/analysis/messageAnalysis.go
@@ -1,5 +1,53 @@
 package analysis
 
-import (
-	Yaml "gobot/src/yaml"
-)
+//Yaml "gobot/src/yaml"
+
+func LevenshteinDistance(str1, str2 string) int {
+	// Convert string parameters to rune arrays to be compatible with non-ASCII
+	runeStr1 := []rune(str1)
+	runeStr2 := []rune(str2)
+
+	// Get and store lenght of these strings
+	runeStr1len := len(runeStr1)
+	runeStr2len := len(runeStr2)
+	if runeStr1len == 0 {
+		return runeStr2len
+	} else if runeStr2len == 0 {
+		return runeStr1len
+	}
+
+	column := make([]int, runeStr1len+1)
+
+	for y := 1; y <= runeStr1len; y++ {
+		column[y] = y
+	}
+	for x := 1; x <= runeStr2len; x++ {
+		column[0] = x
+		lastkey := x - 1
+		for y := 1; y <= runeStr1len; y++ {
+			oldkey := column[y]
+			var i int
+			if runeStr1[y-1] != runeStr2[x-1] {
+				i = 1
+			}
+			column[y] = min(column[y]+1, column[y-1]+1, lastkey+i)
+			lastkey = oldkey
+		}
+	}
+
+	return column[runeStr1len]
+}
+
+func min(a, b, c int) int {
+	if a < b {
+		if a < c {
+			return a
+		}
+	} else {
+		if b < c {
+			return b
+		}
+	}
+
+	return c
+}
